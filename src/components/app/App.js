4fb91f7432305a408105319949a2e1433b201e82
Tatiana <tvtugova1511@gmail.com>
2024-09-10 18:02:56 +0300

    Настроено взаимодействие с API Forismatic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,36 @@
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return (
-    
-  )
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const getData = (url) => {
+        const callbackName = 'jsonpCallback_' + Math.floor(Math.random() * 1000000);
+
+        window[callbackName] = (data) => {
+            setQuote(data.quoteText);
+            setAuthor(data.quoteAuthor || "Автор неизвестен");
+
+            delete window[callbackName];
+            document.body.removeChild(script);
+        };
+
+        const script = document.createElement('script');
+        script.src = `${url}&jsonp=${callbackName}`;
+        document.body.appendChild(script);
+    };
+
+    useEffect(() => {
+        getData("https://api.forismatic.com/api/1.0/?method=getQuote&key=random&format=jsonp&lang=ru");
+    }, []);
+
+    return (
+        <div className="App">
+                <h1 class="">Random Quotes!</h1>
+                <p>{quote}</p>
+                <p>— {author}</p>
+        </div>
+    );
 }
 
 export default App;
